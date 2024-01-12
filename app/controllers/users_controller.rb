8e5e3aa7a3309dd72ad4460a8451c1af92aae46d
dave2281 <dava.bigboy@gmail.com>
2024-01-12 11:15:13 +0200

    added email, pass. user to quest db. views. pass encryption

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,18 @@
 class UsersController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
 
   def show
-    
+    @user = User.new(
+      name: 'John',
+      email: 'john@gmail.com',
+      username: '@johnyboy'
+    )
   end
 end
