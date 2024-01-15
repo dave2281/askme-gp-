6ab2733f3977db777c50681d64d556012832e5b8
dave2281 <dava.bigboy@gmail.com>
2024-01-15 18:14:47 +0200

    some work done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,15 @@ class UsersController < ApplicationController
     @user = User.new(
       name: 'John',
       email: 'john@gmail.com',
-      username: '@johnyboy'
+      username: '@johnyboy',
+      avatar_url: 'https://gravatar.com/avatar/f449c85576a6478bcec3a38e2072e12a?s=400&d=mp&r=x'
     )
+
+    @questions = [
+      Question.new(text: 'How are you?', created_at: Date.parse('20.02.1922')),
+      Question.new(text: 'Fine!', created_at: Date.parse('20.02.1943'))
+    ]
+
+    @new_question = Question.new
   end
 end
