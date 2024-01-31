4e38d3958f5d7def4c2993545e199c971aa5d96d
dave2281 <dava.bigboy@gmail.com>
2024-01-31 13:25:24 +0200

    added functions: add question, reply, delete

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,9 @@ module ApplicationHelper
       asset_path 'avatar.jpg'
     end
   end
+
+  def fa_icon(icon_class)
+    content_tag 'span', '', class: "fa fa-#{icon_class}"
+  end
 end
+ 
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   resources :users, except: [:destroy]
   resources :sessions, only: [:new, :create, :destroy]
-  resources :questions
+  resources :questions, except: [:show, :new, :index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
